8b9f0203c1eadc35eacb082b84b8225f86fc7253
Unmukt Gupta <ug36@cornell.edu>
2020-06-05 09:58:01 -0400

    Bug fixes to number of nodes. Filter values by partition/queue

--- clusterState.py
+++ clusterState.py
@@ -25,7 +25,7 @@ def runList(keys, usr=None):
 	for job in jID:
 		out = shout('scontrol show job %d' % job)
 		res2 = out
-		res = dict(re.findall(r'(\w*)=([\w*{\.,\/\-}]+\w*)', out))
+		res = dict(re.findall(r'(\w*)=([\w*{\.,\/\-}]+\w*)?-*', out))
 		for key in keys:
 			kDict[key].append( res[key] )
 	return kDict
@@ -45,36 +45,51 @@ def fexists(rDict, fname):
 #---------------------------------------------------------------------------------------#
 # Extract user specific entries for given keyword
 #---------------------------------------------------------------------------------------#
-def exUser(rDict, key, usr):
+def exUser(rDict, key, usr, partition=None):
 	# User list from global dictionary
 	USR = np.array( rDict['UserId'] )
 	ind = (USR==usr)
+	# Add filter by partition name
+	if partition != None:
+		PART = np.array( rDict['Partition'] )
+		ind = (ind) & (PART==partition)
 	return np.array( rDict[key] ) [ind]
 #---------------------------------------------------------------------------------------#
 # Main
 #---------------------------------------------------------------------------------------#
 if __name__ == "__main__":
+	partition = 'fe13'
 	# Keywords in scontrol output
-	queries = ['UserId', 'JobName', 'WorkDir', 'NumNodes', 'NumCPUs']
-	rDict = runList(queries)#,  usr='gma74')
+	queries = ['UserId', 'JobName', 'WorkDir', 'NumNodes', 'NumCPUs', 'Partition']
+	rDict = runList(queries)#,  usr='as3833')
 	USR, ucount = np.unique( np.array( rDict['UserId'] ) , return_counts=True)
 #---------------------------------------------------------------------------------------#
 	NumCPUs = []; NumNodes = [];
 	# Loop over the unique set of users running jobs
+	print('USER\t\tnJob\t\tnCPU\t\tnNode\n#%s#' % ('-'*50) )
 	for ui, usr in enumerate(USR):
 		# Corresponding index list
 		# Extend based on keywords
-		NumCPUs.append( exUser(rDict, 'NumCPUs', usr).astype(np.int).sum() )
-		NumNodes.append( exUser(rDict, 'NumNodes', usr).astype(np.int).sum() )
-		print(usr, ucount[ui], NumCPUs[-1], NumNodes[-1] )
+		NumCPUs.append( exUser(rDict, 'NumCPUs', usr, partition=partition).astype(np.int).sum() )
+		# Change to include hyphenations in the node lists
+		_nn = sum([ int(x.split('-')[0]) for x in exUser(rDict, 'NumNodes', usr, partition=partition) ])
+		NumNodes.append( _nn )
+		outList = [ usr, ucount[ui], NumCPUs[-1], NumNodes[-1] ]
+		print( '\t\t'.join(map(str,outList)) )
 #---------------------------------------------------------------------------------------#
 	# Plot
 	fig, ax = plt.subplots()
-	ax.plot(range(len(USR)), NumCPUs, 'r-s', label='NumCPUs')
-	ax.plot(range(len(USR)), NumNodes, 'g-o', label='NumNodes')
+	lcpu = ax.plot(range(len(USR)), NumCPUs, 'r-s', label='NumCPUs')
+	axn = ax.twinx()
+	lnode = axn.plot(range(len(USR)), NumNodes, 'g-o', label='NumNodes')
 #---------------------------------------------------------------------------------------#
 	ax.set_xlabel(r'Users')
 	ax.set_ylabel(r'Resources')
+	axn.set_ylabel(r'Nodes')
+	ax.set_xlim([-0.5,len(USR)-0.5])
+	ax.set_xticks(np.arange(len(USR)))
 	ax.set_xticklabels(USR, rotation=45)
-	ax.legend(loc='best', frameon=False)
+	lns = lcpu+lnode
+	labs = [l.get_label() for l in lns]
+	ax.legend(lns, labs, loc='best', frameon=False)
 	plt.show()
